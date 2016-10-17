3533cc7c3bb8893321ed3e86ebba5d80d332fc8a
Hamlix <nathanaelle@famille-viller.com>
2016-10-17 15:12:33 +0200

    Add Client Class

---
+++ src/fr/paris10/td3/Client/Client.java
@@ -0,0 +1,30 @@
+package fr.paris10.td3.Client;
+
+
+import fr.paris10.td3.Products.Product;
+import fr.paris10.td3.Products.ProdFactory;
+
+/**
+ * Created by galax on 17/10/2016.
+ */
+public class Client {
+    Product prod1;
+    ProdFactory factory;
+
+    public Client(){
+        factory = ProdFactory.getInstance();
+        prod1 = factory.createProduct("ProductA");
+    }
+
+    public void foo(){
+        Product myProd1, myProd2;
+        prod1.perform();
+        myProd1 = factory.createProduct("ProductB");
+        myProd1.perform();
+        myProd2 = factory.createProduct("ProductC");
+        myProd2.perform();
+    }
+
+
+}
+
